db9fb063b8be5caf598722b812996def46ffc68a
AzureXH <42961610+AzureXH@users.noreply.github.com>
2019-05-11 17:57:21 +0800

    Create MoviePlacingRate.java

---
+++ github_pro/src/main/java/com/example/cinema/po/MoviePlacingRate.java
@@ -0,0 +1,22 @@
+package com.example.cinema.po;
+
+public class MoviePlacingRate {
+    private String movieName;
+    private String moviePlacingRate;
+
+    public String getMoviePlacingRate() {
+        return moviePlacingRate;
+    }
+
+    public void setMoviePlacingRate(String moviePlacingRate) {
+        this.moviePlacingRate = moviePlacingRate;
+    }
+
+    public String getMovieName() {
+        return movieName;
+    }
+
+    public void setMovieName(String movieName) {
+        this.movieName = movieName;
+    }
+}
