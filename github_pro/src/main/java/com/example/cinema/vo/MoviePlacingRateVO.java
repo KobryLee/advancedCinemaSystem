ccfd098406b3dbe8701b9fb8fb18ecaaca5a2dac
AzureXH <42961610+AzureXH@users.noreply.github.com>
2019-05-11 17:43:00 +0800

    Update MoviePlacingRateVO.java

--- github_pro/src/main/java/com/example/cinema/vo/MoviePlacingRateVO.java
+++ github_pro/src/main/java/com/example/cinema/vo/MoviePlacingRateVO.java
@@ -7,11 +7,35 @@ package com.example.cinema.vo;
  */
 public class MoviePlacingRateVO {
 	
-	private String moviePlacingRate;
+	
+    /**
+     * 电影名称
+     */
+    private String movieName;
+    /**
+     * 上座率
+     */
+    private String moviePlacingRate;
 
+	
     public MoviePlacingRateVO(String moviePlacingRate) {
         this.moviePlacingRate = moviePlacingRate;
     }
+    public MoviePlacingRateVO(MoviePlacingRate moviePlacingRate) {
+        this.moviePlacingRate = moviePlacingRate.getMoviePlacingRate();
+        this.movieName = moviePlacingRate.getMovieName();
+    }
+	
+    public String getMoviePlacingRate() {
+        return moviePlacingRate;
+    }
+
+    public void setMoviePlacingRate(String moviePlacingRate) {
+        this.moviePlacingRate = moviePlacingRate;
+    }
+}
+
+public class MoviePlacingRateVO {
 
     public String getMoviePlacingRate() {
         return moviePlacingRate;
