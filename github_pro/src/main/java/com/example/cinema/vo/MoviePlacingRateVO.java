6fe8fedc773b28f7aa509d524609bac836e0e6cb
zbcpluto <1078903603@qq.com>
2019-05-11 17:11:16 +0800

    aaa

---
+++ github_pro/src/main/java/com/example/cinema/vo/MoviePlacingRateVO.java
@@ -0,0 +1,23 @@
+package com.example.cinema.vo;
+
+/**
+ * 上座率
+ * @author zbc
+ *
+ */
+public class MoviePlacingRateVO {
+	
+	private String moviePlacingRate;
+
+    public MoviePlacingRateVO(String moviePlacingRate) {
+        this.moviePlacingRate = moviePlacingRate;
+    }
+
+    public String getMoviePlacingRate() {
+        return moviePlacingRate;
+    }
+
+    public void setMoviePlacingRate(String moviePlacingRate) {
+        this.moviePlacingRate = moviePlacingRate;
+    }
+}
