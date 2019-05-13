dd451cf1bc3a6f0f0713fec5596e869ff055cba6
AzureXH <42961610+AzureXH@users.noreply.github.com>
2019-05-13 18:28:53 +0800

    Update MoviePlacingRate.java

--- github_pro/src/main/java/com/example/cinema/po/MoviePlacingRate.java
+++ github_pro/src/main/java/com/example/cinema/po/MoviePlacingRate.java
@@ -1,17 +1,18 @@
 package com.example.cinema.po;
 
-public class MoviePlacingRate {
-	
+public class MoviePlacingAudience {
+    /**
+     * 电影名称
+     */
     private String movieName;
-    private Double moviePlacingRate;
-
-    public Double getMoviePlacingRate() {
-        return moviePlacingRate;
-    }
-
-    public void setMoviePlacingRate(Double d) {
-        this.moviePlacingRate = d;
-    }
+    /**
+     * 当天观看该电影的观众总人数
+     */
+    private Integer audience;
+    /**
+     * 当天该电影的放映场次
+     */
+    private Integer times;
 
     public String getMovieName() {
         return movieName;
@@ -19,5 +20,21 @@ public class MoviePlacingRate {
 
     public void setMovieName(String movieName) {
         this.movieName = movieName;
+    }
+
+    public Integer getAudience() {
+        return audience;
+    }
+
+    public void setAudience(Integer audience) {
+        this.audience = audience;
+    }
+
+    public Integer getTimes() {
+        return times;
+    }
+
+    public void setTimes(Integer times) {
+        this.times = times;
     }
 }
