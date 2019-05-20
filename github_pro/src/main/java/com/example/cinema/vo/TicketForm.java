f86ed91b9407604a52aa1aa706055b215e936782
zbcpluto <37504694+zbcpluto@users.noreply.github.com>
2019-05-20 14:02:10 +0800

    Update TicketForm.java

--- github_pro/src/main/java/com/example/cinema/vo/TicketForm.java
+++ github_pro/src/main/java/com/example/cinema/vo/TicketForm.java
@@ -1,6 +1,10 @@
 package com.example.cinema.vo;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import com.example.cinema.po.ScheduleItem;
 
 /**
  * Created by liying on 2019/4/16.
@@ -11,22 +15,71 @@ public class TicketForm {
      * 用户id
      */
     private int userId;
+    
     /**
      * 排片id
      */
     private int scheduleId;
-
+    
+    /**
+     * 影厅名称
+     */
+    private String hallName;
+    
+    /**
+     * 电影名
+     */
+    private String movieName;
+    /**
+     * 海报url
+     */
+    private String posterUrl;
+    /**
+     * 开始放映时间
+     */
+    private Date startTime;
+    /**
+     * 结束放映时间
+     */
+    private Date endTime;
+    /**
+     * 票价
+     */
+    private double fare;
+    
     private List<SeatForm> seats;
+    
+    /**
+     * 订单状态：
+     * 0：未完成 1：已完成 2:已失效
+     */
+    private int state;
+    
+    public TicketForm() {
+    	seats = new ArrayList<SeatForm>();
+    }
+    
+    public TicketForm(int userId, int scheduleId, ScheduleItem scheduleItem) {
+		this.userId = userId;
+		this.scheduleId = scheduleId;
+		seats = new ArrayList<SeatForm>();
+		this.hallName = scheduleItem.getHallName();
+		this.movieName = scheduleItem.getMovieName();
+		this.startTime = scheduleItem.getStartTime();
+		this.endTime = scheduleItem.getEndTime();
+		this.fare = scheduleItem.getFare();
+	}
 
-    public List<SeatForm> getSeats() {
+    public void addSeat(int columnIndex, int rowIndex) {
+    	seats.add(new SeatForm(columnIndex, rowIndex));
+    }
+
+	public List<SeatForm> getSeats() {
         return seats;
     }
 
     public void setSeats(List<SeatForm> seats) {
         this.seats = seats;
-    }
-
-    public TicketForm() {
     }
 
     public int getUserId() {
@@ -45,6 +98,59 @@ public class TicketForm {
         this.scheduleId = scheduleId;
     }
 
+	public String getHallName() {
+		return hallName;
+	}
 
+	public void setHallName(String hallName) {
+		this.hallName = hallName;
+	}
 
+	public String getMovieName() {
+		return movieName;
+	}
+
+	public void setMovieName(String movieName) {
+		this.movieName = movieName;
+	}
+	
+	public String getPosterUrl() {
+		return posterUrl;
+	}
+
+	public void setPosterUrl(String posterUrl) {
+		this.posterUrl = posterUrl;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	public double getFare() {
+		return fare;
+	}
+
+	public void setFare(double fare) {
+		this.fare = fare;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
 }
